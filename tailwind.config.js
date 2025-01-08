c760c0097aa875d3e7b36fdf496c666c27efcb1f
Jester <jesterdc41@gmail.com>
2025-01-08 12:35:20 +0800

    Fixed asset link errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,11 +13,11 @@ export default {
         jost: ['Jost-Hand', 'sans-serif'],
       },
       backgroundImage: {
-          'graph-light': "url('./images/decorations/graph-light.png')", 
-          'graph-dark': "url('./images/decorations/graph-dark.png')",
-          'flare-light': "url('./images/decorations/flare-strip-light.png')",
-          'dots': "url('./images/decorations/dots.png')",
-          'dots-dark': "url('./images/decorations/dots-dark.png')",
+          'graph-light': "url('/images/decorations/graph-light.png')", 
+          'graph-dark': "url('/images/decorations/graph-dark.png')",
+          'flare-light': "url('/images/decorations/flare-strip-light.png')",
+          'dots': "url('/images/decorations/dots.png')",
+          'dots-dark': "url('/images/decorations/dots-dark.png')",
       },
       backgroundSize: {
           '1000px' : '1200px',
